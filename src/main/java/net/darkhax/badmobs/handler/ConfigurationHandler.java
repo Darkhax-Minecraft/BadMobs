83b50b04a7aae86a1a91b106ade28f36dad36ff8
Darkhax <darklime@live.ca>
2015-07-27 20:52:34 -0600

    Fixed bug preventing mod from working on server. Also added an "enable servside" option that will make this mod serverside only when enabled.

--- src/main/java/net/darkhax/badmobs/handler/ConfigurationHandler.java
+++ src/main/java/net/darkhax/badmobs/handler/ConfigurationHandler.java
@@ -1,9 +1,9 @@
 package net.darkhax.badmobs.handler;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.List;
 
-import scala.actors.threadpool.Arrays;
 import net.darkhax.badmobs.lib.Constants;
 import net.minecraftforge.common.config.Configuration;
 import cpw.mods.fml.client.event.ConfigChangedEvent.OnConfigChangedEvent;
@@ -13,6 +13,7 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 public class ConfigurationHandler {
     
     public static Configuration config;
+    public static boolean serverMode = false;
     public static List<String> bannedEntities;
     
     public ConfigurationHandler(File file) {
@@ -35,7 +36,8 @@ public class ConfigurationHandler {
      */
     private void syncConfigData () {
     
-        bannedEntities = Arrays.asList(config.getStringList("bannedMobs", config.CATEGORY_GENERAL, new String[] {"example1", "example2", "example3"}, "A list of all banned mobs. If a mobs entity name is added to this list, it will not be allowed to spawn in any world. To get the name of an entity, interact with it using the data checker and it's name will be given."));
+        serverMode = config.getBoolean("serverMode", config.CATEGORY_GENERAL, serverMode, "If this is set to true, the mod will be put into server-side mode. This will remove the Entity Data Book from the game, but will allow clients to connect to your server, without having the mod installed.");
+        bannedEntities = Arrays.asList(config.getStringList("bannedMobs", config.CATEGORY_GENERAL, new String[] { "example1", "example2", "example3" }, "A list of all banned mobs. If a mobs entity name is added to this list, it will not be allowed to spawn in any world. To get the name of an entity, interact with it using the data checker and it's name will be given."));
         
         if (config.hasChanged())
             config.save();
