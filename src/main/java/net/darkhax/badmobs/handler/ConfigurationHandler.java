7bd1c803e80eb7987405315f9ad79e8d92a55921
Darkhax <darklime@live.ca>
2015-08-04 16:40:11 -0600

    Implemented KillMode to the 1.7.10 version.

--- src/main/java/net/darkhax/badmobs/handler/ConfigurationHandler.java
+++ src/main/java/net/darkhax/badmobs/handler/ConfigurationHandler.java
@@ -14,6 +14,7 @@ public class ConfigurationHandler {
     
     public static Configuration config;
     public static boolean serverMode = false;
+    public static boolean killMode = true;
     public static List<String> bannedEntities;
     
     public ConfigurationHandler(File file) {
@@ -37,6 +38,7 @@ public class ConfigurationHandler {
     private void syncConfigData () {
     
         serverMode = config.getBoolean("serverMode", config.CATEGORY_GENERAL, serverMode, "If this is set to true, the mod will be put into server-side mode. This will remove the Entity Data Book from the game, but will allow clients to connect to your server, without having the mod installed.");
+        killMode = config.getBoolean("killMode", config.CATEGORY_GENERAL, killMode, "If this is set to true, bad mobs will be killed/deleted when they attempt to spawn. If this is set to false, the mob will not be removed from the world and only the spawning will be prevented, and when the mob is nolonger listed as bad, all previously prevented mobs of that type will spawn.");
         bannedEntities = Arrays.asList(config.getStringList("bannedMobs", config.CATEGORY_GENERAL, new String[] { "example1", "example2", "example3" }, "A list of all banned mobs. If a mobs entity name is added to this list, it will not be allowed to spawn in any world. To get the name of an entity, interact with it using the data checker and it's name will be given."));
         
         if (config.hasChanged())
--- src/main/java/net/darkhax/badmobs/handler/EntitySpawningHandler.java
+++ src/main/java/net/darkhax/badmobs/handler/EntitySpawningHandler.java
@@ -9,7 +9,12 @@ public class EntitySpawningHandler {
     @SubscribeEvent
     public void onEntityJoinWorld (EntityJoinWorldEvent event) {
     
-        if (event.entity != null && !ConfigurationHandler.bannedEntities.isEmpty() && ConfigurationHandler.bannedEntities.contains(EntityList.getEntityString(event.entity)))
+        if (event.entity != null && !ConfigurationHandler.bannedEntities.isEmpty() && ConfigurationHandler.bannedEntities.contains(EntityList.getEntityString(event.entity))) {
+            
+            if (ConfigurationHandler.killMode)
+                event.world.removeEntity(event.entity);
+            
             event.setCanceled(true);
+        }
     }
 }
